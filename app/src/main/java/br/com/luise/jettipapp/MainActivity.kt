b6d930157d1221f30adc60fd673d093d95427b6b
Luis Eduardo <vsluiseduardo01@gmail.com>
2024-07-31 15:41:21 -0400

    feat(Buttons): Criado Widget de botões arredondados.

--- app/src/main/java/br/com/luise/jettipapp/MainActivity.kt
+++ app/src/main/java/br/com/luise/jettipapp/MainActivity.kt
@@ -1,19 +1,27 @@
 package br.com.luise.jettipapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -21,15 +29,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.luise.jettipapp.components.InputField
 import br.com.luise.jettipapp.ui.theme.JetTipAppTheme
+import br.com.luise.jettipapp.widgets.RoundIconButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,7 +48,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetTipAppTheme {
                 MyApp {
-                    TopHeader()
+                    MainContent()
                 }
             }
         }
@@ -90,6 +101,17 @@ fun TopHeader(totalPerPerson: Double = 0.0) {
 @Preview
 @Composable
 fun MainContent() {
+
+    BillForm() { billAmt ->
+        Log.d("Bill", "MainContent: $billAmt")
+    }
+
+
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun BillForm(modifier: Modifier = Modifier, onValChanged: (String) -> Unit = {}) {
     val totalBillState = remember {
         mutableStateOf("")
     }
@@ -97,6 +119,7 @@ fun MainContent() {
     val validState = remember(totalBillState.value) {
         totalBillState.value.trim().isNotEmpty()
     }
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     Surface(
         modifier = Modifier
@@ -105,16 +128,51 @@ fun MainContent() {
         shape = RoundedCornerShape(corner = CornerSize(8.dp)),
         border = BorderStroke(width = 1.dp, color = Color.LightGray)
     ) {
-        Column {
+        Column(
+            modifier = Modifier.padding(all = 6.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
             InputField(
                 valueState = totalBillState,
                 labelId = "Entre com o valor da conta",
                 enabled = true,
                 isSingleLine = true,
                 onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
 
+                    onValChanged(totalBillState.value.trim())
+
+                    keyboardController?.hide()
                 }
             )
+
+            if (validState) {
+                Row(modifier = Modifier.padding(3.dp), horizontalArrangement = Arrangement.Start) {
+                    Text(
+                        text = "Dividir",
+                        modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                    )
+
+                    Spacer(modifier = Modifier.width(120.dp))
+
+                    Row(
+                        modifier = Modifier.padding(horizontal = 3.dp),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        RoundIconButton(imageVector = Icons.Default.Remove, onClick = {
+                            Log.d("BUttons", "BillForm: Removed")
+                        })
+
+                        RoundIconButton(imageVector = Icons.Default.Add, onClick = {
+                            Log.d("BUttons", "BillForm: Add")
+                        })
+                    }
+
+                }
+            } else {
+                Box {}
+            }
         }
     }
 }
